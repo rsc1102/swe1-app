2b34ceb61f4056c10ae50d2d0b4e0deab1058fe6
rsc1102 <rohitchaudhari1102@gmail.com>
2024-10-01 18:34:23 -0400

    updates for eb

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    '.us-east-1.elasticbeanstalk.com']
+    'mysite-dev222.us-east-1.elasticbeanstalk.com/polls/']
 
 
 # Application definition
@@ -110,10 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
