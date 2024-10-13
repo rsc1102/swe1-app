e5129e1d3217570e90fd499b818e4232393906d8
rsc1102 <rohitchaudhari1102@gmail.com>
2024-10-13 13:42:08 -0400

    updated file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
-urlpatterns = [path("polls/", include("polls.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", lambda request: redirect("polls:index"), name="landing_page"),
+]
